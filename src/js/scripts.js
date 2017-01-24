72289ad91f30b2861f2c3768476f97ee00694c0b
Hrvoje Milas <hrvoje.milas@omega-software.hr>
2017-01-24 21:16:12 +0100

    Added loader. Code cleanup

---
+++ src/js/scripts.js
@@ -0,0 +1,37 @@
+require("jquery.easing")
+require('bootstrap-loader');
+require("font-awesome-loader");
+require("../css/style.css");
+
+
+$(function() {
+    var offset = 32;
+    $('#main-nav li a').click(function(e) {
+        e.preventDefault();
+        var href = $(this).attr('href');
+
+        $(href)[0].scrollIntoView();
+        scrollBy(0, offset);
+    });
+
+    $(window).scroll(function() {
+        var $navbar = $(".navbar");
+        if ($navbar.offset().top > 50) {
+            $navbar.addClass("small-header");
+        } else {
+            $navbar.removeClass("small-header");
+        }
+    });
+
+    $(window).on("load", function() {
+        $("#status").fadeOut(1200).fadeOut('slow', function() {
+            $("#loader").remove();      
+            $("body").delay(0).css({'overflow':'visible'});      
+        });        
+    });
+    
+    window.sr = ScrollReveal();
+    sr.reveal('#web-app-container', { delay: 300});
+    sr.reveal('#consulting-container', { delay: 300 });
+    sr.reveal('#company-container', { delay: 300 });
+});
